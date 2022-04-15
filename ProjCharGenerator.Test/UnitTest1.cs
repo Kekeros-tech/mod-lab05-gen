24b8b9dc25a22af253d1faefdd2adf55c5742457
Kekeros-tech <69265505+Kekeros-tech@users.noreply.github.com>
2022-04-15 18:19:43 +0300

    Encoding fix

--- ProjCharGenerator.Test/UnitTest1.cs
+++ ProjCharGenerator.Test/UnitTest1.cs
@@ -1,7 +1,9 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+ï»¿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using generator;
 using System.Collections.Generic;
 using System.IO;
+
+//å…šçš„è€»è¾±
 
 namespace ProjCharGenerator.Test
 {
@@ -11,111 +13,111 @@ namespace ProjCharGenerator.Test
         [TestMethod]
         public void TestMethod1()
         {
-            BigramGenerator generator = new BigramGenerator("../../sources/BG.txt");
+            BigramGenerator generator = new BigramGenerator("../../../../sources/BG.txt");
             int rows = generator.chWeights.GetUpperBound(0) + 1;
             Assert.IsTrue(rows == generator.syms.Length);
         }
         [TestMethod]
         public void TestMethod2()
         {
-            BigramGenerator generator = new BigramGenerator("../../sources/BG.txt");
-            int weightOfChar = generator.getSumOfWeightOfCurrentCh('ï');
+            BigramGenerator generator = new BigramGenerator("../../../../sources/BG.txt");
+            int weightOfChar = generator.getSumOfWeightOfCurrentCh('Ð¿');
             Assert.IsTrue(weightOfChar == 134);
         }
         [TestMethod]
         public void TestMethod3()
         {
-            BigramGenerator generator = new BigramGenerator("../../sources/BG.txt");
-            List<char> possibleValues = new List<char>("àåèëíîðñóûÿ".ToCharArray());
-            char receivedValue = generator.getNextCh('ï');
+            BigramGenerator generator = new BigramGenerator("../../../../sources/BG.txt");
+            List<char> possibleValues = new List<char>("Ð°ÐµÐ¸Ð»Ð½Ð¾Ñ€ÑÑƒÑ‹Ñ".ToCharArray());
+            char receivedValue = generator.getNextCh('Ð¿');
             Assert.IsTrue(possibleValues.Contains(receivedValue));
         }
         [TestMethod]
         public void TestMethod4()
         {
-            BigramGenerator generator = new BigramGenerator("../../sources/BG.txt");
+            BigramGenerator generator = new BigramGenerator("../../../../sources/BG.txt");
             Program.writeToFile("tests/BigramGeneratorTest.txt", generator);
-            string[] s = File.ReadAllLines("../../work_results/tests/BigramGeneratorTest.txt");
+            string[] s = File.ReadAllLines("../../../../work_results/tests/BigramGeneratorTest.txt");
             string receivedValue = s[0];
             Assert.IsTrue(receivedValue.Length >= 1000);
         }
         [TestMethod]
         public void TestMethod5()
         {
-            WordGenerator generator1 = new WordGenerator("../../sources/Words.txt");
+            WordGenerator generator1 = new WordGenerator("../../../../sources/Words.txt");
             int count = generator1.wordsInSample.Count;
             Assert.IsTrue(count == 100);
         }
         [TestMethod]
         public void TestMethod6()
         {
-            string[] strAfterSplit = { "è", "7416716" };
+            string[] strAfterSplit = { "Ð¸", "7416716" };
             Word expected = new Word(strAfterSplit);
-            WordGenerator generator = new WordGenerator("../../sources/Words.txt");
+            WordGenerator generator = new WordGenerator("../../../../sources/Words.txt");
             Word comparison = generator.wordsInSample[0];
             Assert.IsTrue((expected.word == comparison.word) && (expected.weight == comparison.weight));
         }
         [TestMethod]
         public void TestMethod7()
         {
-            WordGenerator generator = new WordGenerator("../../sources/Words.txt");
+            WordGenerator generator = new WordGenerator("../../../../sources/Words.txt");
             Program.writeToFile("tests/WordGeneratorTest.txt", generator);
-            string[] s = File.ReadAllLines("../../work_results/tests/WordGeneratorTest.txt");
+            string[] s = File.ReadAllLines("../../../../work_results/tests/WordGeneratorTest.txt");
             string[] receivedValue = s[0].Split(" ");
             Assert.IsTrue(receivedValue.Length >= 1000);
         }
         [TestMethod]
         public void TestMethod8()
         {
-            WordBigramGenerator generator = new WordBigramGenerator("../../sources/WB.txt");
+            WordBigramGenerator generator = new WordBigramGenerator("../../../../sources/WB.txt");
             int count = generator.wordsInSample.Count;
             Assert.IsTrue(count == 100);
         }
         [TestMethod]
         public void TestMethod9()
         {
-            string[] strAfterSplit = { "è", "íå", "201352" };
+            string[] strAfterSplit = { "Ð¸", "Ð½Ðµ", "201352" };
             WordBigram expected = new WordBigram(strAfterSplit);
-            WordBigramGenerator generator = new WordBigramGenerator("../../sources/WB.txt");
+            WordBigramGenerator generator = new WordBigramGenerator("../../../../sources/WB.txt");
             WordBigram comparison = generator.wordsInSample[0];
             Assert.IsTrue((expected.firstWord == comparison.firstWord) && (expected.secondWord == comparison.secondWord) && (expected.weight == comparison.weight));
         }
         [TestMethod]
         public void TestMethod10()
         {
-            WordBigramGenerator generator = new WordBigramGenerator("../../sources/WB.txt");
+            WordBigramGenerator generator = new WordBigramGenerator("../../../../sources/WB.txt");
             List<WordBigram> wordBigrams;
             int sumOfList;
-            generator.getWordsWhichStartsWithLastWord("â", out wordBigrams, out sumOfList);
+            generator.getWordsWhichStartsWithLastWord("Ð²", out wordBigrams, out sumOfList);
             Assert.IsTrue(wordBigrams.Count == 8 && sumOfList == 389188);
         }
         [TestMethod]
         public void TestMethod11()
         {
-            WordBigramGenerator generator = new WordBigramGenerator("../../sources/WB.txt");
+            WordBigramGenerator generator = new WordBigramGenerator("../../../../sources/WB.txt");
             List<WordBigram> wordBigrams;
             int sumOfList;
-            generator.getWordsWhichStartsWithLastWord("â", out wordBigrams, out sumOfList);
+            generator.getWordsWhichStartsWithLastWord("Ð²", out wordBigrams, out sumOfList);
             WordBigram result = generator.getWBByRandom(wordBigrams, 1000000000);
             Assert.IsTrue(result == null);
         }
         public void TestMethod12()
         {
-            WordBigramGenerator generator = new WordBigramGenerator("../../sources/WB.txt");
+            WordBigramGenerator generator = new WordBigramGenerator("../../../../sources/WB.txt");
             List<WordBigram> wordBigrams;
             int sumOfList;
-            generator.getWordsWhichStartsWithLastWord("â", out wordBigrams, out sumOfList);
+            generator.getWordsWhichStartsWithLastWord("Ð²", out wordBigrams, out sumOfList);
             WordBigram result = generator.getWBByRandom(wordBigrams, 15000);
-            string[] strAfterSplit = { "â", "òîì", "89842" };
+            string[] strAfterSplit = { "Ð²", "Ñ‚Ð¾Ð¼", "89842" };
             WordBigram expected = new WordBigram(strAfterSplit);
             Assert.IsTrue((result.firstWord == expected.firstWord) && (result.secondWord == expected.secondWord) && (result.weight == expected.weight));
         }
         [TestMethod]
         public void TestMethod13()
         {
-            WordBigramGenerator generator = new WordBigramGenerator("../../sources/WB.txt");
+            WordBigramGenerator generator = new WordBigramGenerator("../../../../sources/WB.txt");
             Program.writeToFile("tests/WordBigramGenerator.txt", generator);
-            string[] s = File.ReadAllLines("../../work_results/tests/WordBigramGenerator.txt");
+            string[] s = File.ReadAllLines("../../../../work_results/tests/WordBigramGenerator.txt");
             string[] receivedValue = s[0].Split(" ");
             Assert.IsTrue(receivedValue.Length >= 1000);
         }
